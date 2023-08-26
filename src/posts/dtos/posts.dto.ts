98d78e486619909502cb1164867782bccf58bdbc
Wesley Michael <wesleymichaelpereira@hotmail.com>
2023-08-26 05:24:27 -0300

    feat: make 'image' field optional in PostsDTO

--- src/posts/dtos/posts.dto.ts
+++ src/posts/dtos/posts.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class PostsDTO {
   @IsString()
@@ -10,5 +10,6 @@ export class PostsDTO {
   text: string;
 
   @IsString()
+  @IsOptional()
   image: string;
 }
